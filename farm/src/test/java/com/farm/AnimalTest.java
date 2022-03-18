7b0958904f0e99caf74e4b7c381f6b4ee428b4e7
iMMoCodes <matti-makela@hotmail.com>
2022-03-18 12:22:09 +0200

    Added pdf

--- farm/src/test/java/com/farm/AnimalTest.java
+++ farm/src/test/java/com/farm/AnimalTest.java
@@ -1,6 +1,8 @@
 package com.farm;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -8,7 +10,9 @@ public class AnimalTest {
     @Test
     public void testGetFriendList() {
         Animal animal = new Animal("Jerry", "Cheese", "Tom");
+        assertFalse("Friendlist is not empty", animal.getFriendList().isEmpty());
         assertEquals("Friendlist length is one at the beginning", 1, animal.getFriendList().size());
+        assertTrue("Friendlist contains best friend", animal.getFriendList().contains(animal.getBestFriendForever()));
         ;
     }
 }
